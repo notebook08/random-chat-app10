e576378222542c21acaa6cda2d62e83ad26e48dd
2ndoneda <aasthachandankar@gmail.com>
2025-07-05 21:05:57 +0530

    Fix missing client directory structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,32 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { SocketProvider } from "./context/SocketProvider.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import { ThemeProvider } from "./components/theme-provider.tsx";
-import { PremiumProvider } from "./context/PremiumProvider.tsx";
-import { CoinProvider } from "./context/CoinProvider.tsx";
-import { FriendsProvider } from "./context/FriendsProvider.tsx";
-import { HelmetProvider } from "react-helmet-async";
-
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HelmetProvider>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <PremiumProvider>
-          <CoinProvider>
-            <FriendsProvider>
-              <BrowserRouter>
-                <SocketProvider>
-                  <App />
-                </SocketProvider>
-              </BrowserRouter>
-            </FriendsProvider>
-          </CoinProvider>
-        </PremiumProvider>
-      </ThemeProvider>
-    </HelmetProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
